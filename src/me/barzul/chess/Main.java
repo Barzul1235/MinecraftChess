daa47169619abf00653232073608a941bada1aeb
Barzul1235 <niklas-frederik@web.de>
2021-03-24 14:37:48 +0100

    Initial commit

---
+++ src/me/barzul/chess/Main.java
@@ -0,0 +1,11 @@
+package me.barzul.chess;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("-> Chess-Plugin enabled!");
+    }
+}
